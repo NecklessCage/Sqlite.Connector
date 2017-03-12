c6193c96dbf7374fff5a6b700e15c375379f7b2a
Neckless Cage <htetaung04@gmail.com>
2017-03-12 23:03:57 +0630

    Select Query Builder

---
+++ Sql.Query/KeyWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sql.Query
+{
+    static class KeyWord
+    {
+        public const string SPACE = " ";
+        public const string COMMA = ",";
+        public const string SELECT = "SELECT";
+        public const string INSERT = "INSERT";
+        public const string UPDATE = "UPDATE";
+        public const string DELETE = "DELETE";
+        public const string FROM = "FROM";
+        public const string WHERE = "WHERE";
+        public const string AND = "AND";
+    }
+}
--- Wpf_Net_4/MainWindow.xaml.cs
+++ Wpf_Net_4/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 
+using Sql.Query;
 using Sqlite.Connector;
 
 namespace Wpf_Net_4
@@ -12,32 +13,58 @@ namespace Wpf_Net_4
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        readonly string conStrReadonly = Connector.ConnectionString(dataSource: @"W:\Databases\Sqlite\pos.db"
+                                    , password: "htetaung"
+                                    , isReadonly: true
+                                    , pooling: true
+                                    , version: 3);
+
         public MainWindow()
         {
             InitializeComponent();
 
-            string constr = Connector.ConnectionString(dataSource: @"W:\Databases\Sqlite\pos.db"
-                                , password: "htetaung"
-                                , isReadonly: false
-                                , pooling: true
-                                , version: 3);
-            MessageBox.Show(
-                Connector.Dml.NonQuery(constr
-                    , @"UPDATE [UserRole] SET [UserRoleName]='Sales Manager' WHERE [UserRoleDescription]='-';")
-                .ToString());
+            var b = new Builder.SELECTBuilder();
+            b.Columns("UserKey", "UserName");
+            b.Table("User");
+            b.WhereConjunction(Builder.BinaryCompare("UserKey", "0", Builder.BinaryOperator.Equal)
+                , Builder.BinaryCompare("UserName", "aunggyi", Builder.BinaryOperator.Equal));
+            string q = "";
+            if (b.Query(out q))
+            {
+                MessageBox.Show(q);
+                var dt = Connector.Dml.Select(conStrReadonly, q);
+                dtgHola.ItemsSource = dt.DefaultView;
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
 
-            constr = Connector.ConnectionString(dataSource: @"W:\Databases\Sqlite\pos.db"
-                                , password: "htetaung"
-                                , isReadonly: true
-                                , pooling: true
-                                , version: 3);
-            var dt = Connector.Dml.Select(constr,
-                        @"SELECT * FROM [UserRole];");
-            dtgHola.ItemsSource = dt.DefaultView;
+            if (false)
+            {
+                string constr = Connector.ConnectionString(dataSource: @"W:\Databases\Sqlite\pos.db"
+                                    , password: "htetaung"
+                                    , isReadonly: false
+                                    , pooling: true
+                                    , version: 3);
+                MessageBox.Show(
+                    Connector.Dml.NonQuery(constr
+                        , @"UPDATE [UserRole] SET [UserRoleName]='Sales Manager' WHERE [UserRoleDescription]='-';")
+                    .ToString());
 
-            var v = Connector.Dml.Scalar<long>(constr
-                    , @"SELECT [UserRoleKey] FROM [UserRole] WHERE [UserRoleName]='Office Manager';");
-            MessageBox.Show(null != v ? v.Value.ToString() : "NULL");
+                constr = Connector.ConnectionString(dataSource: @"W:\Databases\Sqlite\pos.db"
+                                    , password: "htetaung"
+                                    , isReadonly: true
+                                    , pooling: true
+                                    , version: 3);
+                var dt = Connector.Dml.Select(constr,
+                            @"SELECT * FROM [UserRole];");
+                dtgHola.ItemsSource = dt.DefaultView;
+
+                var v = Connector.Dml.Scalar<long>(constr
+                        , @"SELECT [UserRoleKey] FROM [UserRole] WHERE [UserRoleName]='Office Manager';");
+                MessageBox.Show(null != v ? v.Value.ToString() : "NULL");
+            }
         }
     }
 }
